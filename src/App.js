4ce5c5266e2fc05a4d00dc722ad26cb33390ad8e
Alvaro Martinez <alvaromfdv@gmail.com>
2023-05-25 18:53:20 +0200

    done

--- src/App.js
+++ src/App.js
@@ -5,10 +5,38 @@ import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
 import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
 
 function App() {
-  const [allCountries, setAllCountries] = useState(countriesData);
+  // const [allCountries, setAllCountries] = useState(countriesData);
+  const [allCountries, setAllCountries] = useState(null);
   // console.log(allCountries);
+
+  const getData = async () => {
+
+    try {
+      const response = await axios.get("https://ih-countries-api.herokuapp.com/countries")
+      console.log(response)
+      
+      setAllCountries(response.data)
+    } catch (error){
+      console.log(error)
+    }
+
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (allCountries === null) {
+    return (
+      <div>
+        <h3>... Loading</h3>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <Navbar />
--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
 
 function CountriesList({ allCountries }) {
   // console.log('allCountries -> ', allCountries);
@@ -7,19 +8,21 @@ function CountriesList({ allCountries }) {
     <div>
       <h4>Countries List</h4>
       {allCountries.map((eachCountry) => {
-        {/* console.log(eachCountry); */}
+        {
+          /* console.log(eachCountry); */
+        }
         return (
-          <div key={eachCountry.alpha3Code}>
+          <Card key={eachCountry.alpha3Code} className="CardContainer">
             <Link to={`/country/${eachCountry.alpha3Code}`}>
-              <div>
-                <img
-                  src={`https://flagpedia.net/data/flags/icon/72x54/${eachCountry.alpha2Code.toLowerCase()}.png`}
-                  alt="bandera"
-                />
-                <p>{eachCountry.name.common}</p>
-              </div>
+              <Card.Img
+                src={`https://flagpedia.net/data/flags/icon/72x54/${eachCountry.alpha2Code.toLowerCase()}.png`}
+                alt="bandera"
+              />
+              <Card.Title className="titulopais">
+                {eachCountry.name.common}
+              </Card.Title>
             </Link>
-          </div>
+          </Card>
         );
       })}
     </div>
